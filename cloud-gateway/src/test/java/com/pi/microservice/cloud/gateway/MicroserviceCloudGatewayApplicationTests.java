5af6ffd6205c863926aadd5fae410d4fb0753202
wirecobweb <wirecobweb@gmail.com>
2020-07-14 17:37:50 +0200

    cloud

---
+++ cloud-gateway/src/test/java/com/pi/microservice/cloud/gateway/MicroserviceCloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pi.microservice.cloud.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceCloudGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
